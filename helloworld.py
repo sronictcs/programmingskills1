2d9f172532291ae249e83e5b2cf521ab463413e1
St Robert of Newminster Catholic School and Sixth Form ICT and CS Dept <47178190+sronictcs@users.noreply.github.com>
2020-09-17 18:54:57 +0100

    Update helloworld.py
    Added comments detail.

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,10 @@
+#Comments
+'''A comment is just a line with anything written on, but isn't run. They are usually used to increase readability and explain parts of a program.
+
+When you put a #, everything after that on that line will not be run (they will be a comment).'''
+
+#e.g.
+
 #This is a print statement
 print("hello world")
 

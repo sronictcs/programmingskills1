8b754f2a91dc7842bcda7b880d1f395202a3c8ec
St Robert of Newminster Catholic School and Sixth Form ICT and CS Dept <47178190+sronictcs@users.noreply.github.com>
2020-09-19 15:49:59 +0100

    Update namecase.py
    updated task numbering

--- namecase.py
+++ namecase.py
@@ -1,9 +1,9 @@
 #2-4 name cases
-'''2-4. Name Cases: Use a variable to represent a person’s name, and then print
+'''1-10. Name Cases: Use a variable to represent a person’s name, and then print
 that person’s name in lowercase, uppercase, and title case.
 '''
 
-#2-5. Famous Quote:
+#1-11. Famous Quote:
 '''Find a quote from a famous person you admire. Print the
 quote and the name of its author. Your output should look something like the
 following, including the quotation marks:
@@ -11,7 +11,7 @@ Albert Einstein once said, “A person who never made a
 mistake never tried anything new.”
 '''
       
-#2-6. Famous Quote 2:
+#1-12. Famous Quote 2:
 '''Repeat Exercise 2-5, but this time, represent the
 famous person’s name using a variable called famous_person. Then compose
 your message and represent it with a new variable called message. Print your

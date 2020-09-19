c3877667bcd91461bdd4a6aaec7b13c830834f31
St Robert of Newminster Catholic School and Sixth Form ICT and CS Dept <47178190+sronictcs@users.noreply.github.com>
2020-09-19 15:49:04 +0100

    Update apostrophe.py
    added ralph wiggum quote

--- apostrophe.py
+++ apostrophe.py
@@ -2,3 +2,8 @@
 message = "One of Python's strengths is its diverse community."
 
 print(message)
+
+#1-9 create a variable that holds some speech in speech marks e.g.
+#"Me fail English? That’s unpossible." said Ralph Wiggum
+#then print it use single quotes.
+
